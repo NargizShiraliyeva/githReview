fba3185ac7e677bcd5044750c16cf28265564c74
NargizShiraliyeva <shiraliyeva.nargiz@gmail.com>
2022-05-31 21:56:40 -0400

    2nd commit

--- src/gitReview/gitReview01.java
+++ src/gitReview/gitReview01.java
@@ -2,4 +2,9 @@ package gitReview;
 
 public class gitReview01 {
 
+	public static void main(String[] args) {
+		System.out.println("Yes you can do it");
+		System.out.println("You will succeed");
+		System.out.println("You will succeed");
+	}
 }
